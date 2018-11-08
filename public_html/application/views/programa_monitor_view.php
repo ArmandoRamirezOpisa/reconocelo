4feda5da63fbff214e0689fba29c00a7f12e176c
Armando Ramirez <armando.ramirez@opisa.com>
2018-11-08 14:46:13 -0600

    parche temporal

--- public_html/application/views/programa_monitor_view.php
+++ public_html/application/views/programa_monitor_view.php
@@ -24,9 +24,80 @@ include 'home_monitor_view_header.php';
                     </tr>
                 </thead>
                 <tbody>
-                <?php
+               <?php
 
-                    if ($programa){
+                    $puntosCirculantes = "0";
+
+                    $fecha05 = "2018 05";//
+                    $fecha06 = "2018 06";
+                    $fecha07 = "2018 07";
+                    $fecha08 = "2018 08";
+                    $fecha09 = "2018 09";
+                    $fecha10 = "2018 10";
+
+                    $canje1 = "38170590";//
+                    $canje2 = "21719269";
+                    $canje3 = "11616206";
+                    $canje4 = "48736764";
+                    $canje5 = "14769473";
+                    $canje6 = "13839401";
+
+                    $deposito = "0";
+                    $deposito1 = "15525000";//
+                    $deposito2 = "18975000";
+                    $deposito3 = "8625000";
+                    $deposito4 = "31050000";
+                    $deposito5 = "10350000";
+
+                    echo '<tr>';
+                        echo '<td>'.$fecha05.'</td>';
+                        echo '<td>'.$deposito1.'</td>';
+                        echo '<td>'.$canje1.'</td>';
+                        $puntosCirculantes = $puntosCirculantes + ($deposito1 - $canje1);
+                        echo '<td>'.$puntosCirculantes.'</td>';
+                    echo '</tr>';
+
+                    echo '<tr>';
+                        echo '<td>'.$fecha06.'</td>';
+                        echo '<td>'.$deposito2.'</td>';
+                        echo '<td>'.$canje2.'</td>';
+                        $puntosCirculantes = $puntosCirculantes + ($deposito2 - $canje2);
+                        echo '<td>'.$puntosCirculantes.'</td>';
+                    echo '</tr>';
+
+                    echo '<tr>';
+                        echo '<td>'.$fecha07.'</td>';
+                        echo '<td>'.$deposito3.'</td>';
+                        echo '<td>'.$canje3.'</td>';
+                        $puntosCirculantes = $puntosCirculantes + ($deposito3 - $canje3);
+                        echo '<td>'.$puntosCirculantes.'</td>';
+                    echo '</tr>';
+
+                    echo '<tr>';
+                        echo '<td>'.$fecha08.'</td>';
+                        echo '<td>'.$deposito4.'</td>';
+                        echo '<td>'.$canje4.'</td>';
+                        $puntosCirculantes = $puntosCirculantes + ($deposito4 - $canje4);
+                        echo '<td>'.$puntosCirculantes.'</td>';
+                    echo '</tr>';
+
+                    echo '<tr>';
+                        echo '<td>'.$fecha09.'</td>';
+                        echo '<td>'.$deposito5.'</td>';
+                        echo '<td>'.$canje5.'</td>';
+                        $puntosCirculantes = $puntosCirculantes + ($deposito5 - $canje5);
+                        echo '<td>'.$puntosCirculantes.'</td>';
+                    echo '</tr>';
+
+                    echo '<tr>';
+                        echo '<td>'.$fecha10.'</td>';
+                        echo '<td>'.$deposito.'</td>';
+                        echo '<td>'.$canje6.'</td>';
+                        $puntosCirculantes = $puntosCirculantes + ($deposito - $canje6);
+                        echo '<td>'.$puntosCirculantes.'</td>';
+                    echo '</tr>';
+
+                    /*if ($programa){
 
                         $puntosCirculantes = 0;
                         $canjes = 0;
@@ -49,7 +120,7 @@ include 'home_monitor_view_header.php';
                             <td>--</td>
                             <td>--</td>
                         </tr>';
-                    }
+                    }*/
                 ?>
                 </tbody>
                 <tfoot>
