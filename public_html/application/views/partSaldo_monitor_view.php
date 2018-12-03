f6f678376ad06511ad4bae745e54136a86f5a3f7
Armando Ramirez <armando.ramirez@opisa.com>
2018-12-03 10:49:16 -0600

    modal participantes

--- public_html/application/views/partSaldo_monitor_view.php
+++ public_html/application/views/partSaldo_monitor_view.php
@@ -32,15 +32,15 @@
                                 <td>'.$row["eMail"].'</td>
                                 <td>'.$row["SaldoActual"].'</td>
                                 <td>
-                                    <button id='.$row["codParticipante"].' type="button" class="btn btn-link" data-toggle="modal" data-target="#exampleModal">
+                                    <button id='.$row["codParticipante"].' type="button" class="btn btn-link" data-toggle="modal" data-target="#modalParticipante">
                                         Información
                                     </button>
+
                                 </td>
                             </tr>';
                         }
                     }else{
                         echo '<tr>
-                            <td>--</td>
                             <td>--</td>
                             <td>--</td>
                             <td>--</td>
@@ -62,3 +62,24 @@
                         </tr>
                     </tfoot>
                 </table>
+
+                <!-- Modal -->
+                <div class="modal fade bd-example-modal-lg" id="modalParticipante" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                ...
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
