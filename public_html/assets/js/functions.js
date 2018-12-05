6886ac923b72396331705ac869028297b0e06b8f
Armando Ramirez <armando.ramirez@opisa.com>
2018-12-05 10:08:18 -0600

    js

--- public_html/assets/js/functions.js
+++ public_html/assets/js/functions.js
@@ -16,7 +16,6 @@ function loadSection(controller, divSel) //Controlador,Div en el que se desplieg
         ifModified: false,
         processData: true,
         success: function(result) {
-            //console.log(result);
             if (result == "0") {
                 console.log("Expiro");
                 window.location.reload();
@@ -49,13 +48,7 @@ var formatNumber = {
     }
 }
 
-
-
-
-
-
 function addItemOrder(idProduct, name, puntos) {
-    //console.log("Agregar:"+idProduct+"-"+name+"-"+puntos);
     var exist = 0;
     //Arma array de productos seleccionados, 
     //si el id seleccionado ya se encuentra en el array lo elimina del mismo.
@@ -70,12 +63,10 @@ function addItemOrder(idProduct, name, puntos) {
             "puntos": puntos
         }];
 
-        //console.log("Agrega id "+contOrder[0].id+" al array de la orden. Cantidad: "+contOrder[0].cantidad+" Nombre:"+contOrder[0].nombre+" Puntos:"+contOrder[0].puntos);
     } else {
         //A partir del segundo elemento se verifica si existe en el array, si existe se le suma 1 a la cantidad.
         $.each(contOrder, function(k, v) {
             if (v.id == idProduct) {
-                //console.log("Existe el id "+v.id+", se suma uno a la cantidad:"+v.cantidad);
                 v.cantidad = parseInt(v.cantidad) + 1;
                 v.puntos = parseInt(v.puntos) + puntos;
                 exist = 1;
@@ -84,7 +75,6 @@ function addItemOrder(idProduct, name, puntos) {
 
         //Si no existe en el array se agrega un nuevo elemento con cantidad 1
         if (exist == 0) {
-            //console.log("Se inserta el id "+idProduct+" como un nuevo elemento al array");
             contOrder.push({
                 "id": idProduct,
                 "cantidad": "1",
@@ -95,18 +85,15 @@ function addItemOrder(idProduct, name, puntos) {
     }
     loadSection("cart_controller/showContentCart/", "dvContAw");
     $.notify("Se ha agregado el producto a su orden", "success");
-    //console.log("Total de elementos en array: "+contOrder.length);
 }
 
 function refreshCar() {
     //Actualiza las cantidades de los productos en el array
-    //console.log("Refresca array");
     $.each(contOrder, function(k, v) {
         if ($("#in" + v.id).val() == 0) {
             alert("La cantidad indicada debe ser mayor a cero");
         } else {
             v.cantidad = $("#in" + v.id).val();
-            //console.log("Asigna al id: "+v.id+ "la cantidad: "+v.cantidad);
         }
     });
     showOrderContent(0);
@@ -124,11 +111,9 @@ function deleteItem(item) {
         dangerMode: true,
     }).then(function(isConfirm) {
         if (isConfirm) {
-            //console.log("Eliminara: "+item);
             $.each(contOrder, function(k, v) {
                 if (item == v.id) {
                     contOrder.splice(k, 1);
-                    //console.log("Elimino: "+item);
                     loadSection("cart_controller/showContentCart/", "dvContAw");
                     $.notify("Se ha eliminado el producto de su orden", "success");
                     return false;
@@ -138,8 +123,6 @@ function deleteItem(item) {
                 $('#dvContOrder').modal('hide')
                 $("#dvCar").html('');
                 bloqueaCombos(0);
-                //console.log("Orden elimiada");
-                //console.log("No.Contenido: "+contOrder.length);
             } else {
                 showOrderContent(0);
             }
@@ -147,17 +130,6 @@ function deleteItem(item) {
             //  swal("Cancelled", "Your imaginary file is safe :)", "error");
         }
     });
-
-
-
-
-
-    //var r=confirm("Eliminara el producto seleccionado ¿Desae continuar?")
-    //if (r==true)
-    //{
-
-
-    //}
 }
 
 function delArray() {
@@ -165,7 +137,6 @@ function delArray() {
     $.each(contOrder, function(k, v) {
         contOrder.splice(k, 1);
     });
-    //confirmDelArray();
 }
 
 function showDet(id) {
@@ -184,65 +155,32 @@ function sendDataAjaxDuda(idCanjeArticulo, NombreCanjeArticulo, mensaje, tipo) {
         },
         success: function(response) {
             if (response) {
-                //  $.notify("Envio de tu mensaje finalizado correctamente.", "success");
                 swal("Mensaje enviado", "Envio de tu mensaje finalizado correctamente.", "success");
                 document.getElementById("mensaje").value = "";
-
-                //   location.reload();
-                //delArray();//Elimina el contenido del array
-                //  setTimeout(function(){location.href="https://www.reconocelo.com.mx";}, 3000);
             } else {
-
                 swal("Envio de Mensaje", "Ha ocurrido un error al enviar tu mensaje.", "warning");
-                // $.notify("Ha ocurrido un error al enviar tu mensaje.", "error");
-                // swal("Mensaje enviado", "Envio de tu mensaje finalizado correctamente.", "success");  
-                //  
-                //  $("#btnGenCanje").show();
-                // $("#lblProc").hide();
             }
         },
         error: function(x, e) {
             swal("Envio de Mensaje", "Ocurrio un error al enviar su mensaje", "warning");
-            // alert("Ocurrio un error al enviar su duda" + e.messager);
-            //   $("#btnGenCanje").show();
-            //  $("#lblProc").hide();
         }
     });
-
 
 }
 
 function enviarPregunta(idCanjeArticulo, NombreCanjeArticulo, mensaje, tipo) {
-
     switch (tipo) {
         case 1:
-            //  alert("aaa");
-            //   //console.log('Procesando, espere por favor...');
-            // alert(idCanjeArticulo+NombreCanjeArticulo+mensaje); 
-            //  console.log('1');       
             sendDataAjaxDuda(idCanjeArticulo, NombreCanjeArticulo, mensaje, tipo);
-
             break;
         case 2:
-            //    alert("aaa");
-            //    alert(idCanjeArticulo+NombreCanjeArticulo+mensaje); 
-            //         console.log('2');
             sendDataAjaxDuda(idCanjeArticulo, NombreCanjeArticulo, mensaje, tipo);
             break;
-
-
         case 3:
-            // alert("aaa");
-            //  alert(idCanjeArticulo+NombreCanjeArticulo+mensaje);  
-            //       console.log('3');
             sendDataAjaxDuda(idCanjeArticulo, NombreCanjeArticulo, mensaje, tipo);
             break;
     }
-    // alert("enviado");*/
 }
-
-
-
 
 function sendCanje($ptsUser, $ptsCanje) {
     $('#btnGenCanje').attr('disabled', false);
@@ -250,78 +188,53 @@ function sendCanje($ptsUser, $ptsCanje) {
     periodoCanjes = 1;
     if (validaCampos()) {
         if (periodoCanjes == 1) {
-            //$("#btnGenCanje").hide();
-            // $("#lblProc").show();
 
             if ($ptsUser >= $ptsCanje) {
                 if (contOrder.length > 0) {
 
                     var jsonString = JSON.stringify(contOrder); //Pasa array a formato JSON
                     var address = $("#frmCanjeDir").serializeArray();
-                    //data:{"data": jsonString,"ptsCanje":$ptsCanje,"address":$("#frmCanjeDir").serialize()},
                     $.ajax({
                         type: 'POST',
                         url: "canje_controller/addCanje",
                         dataType: "json",
                         data: { "data": jsonString, "ptsCanje": $ptsCanje, "address": address },
                         beforeSend: function() {
-                            //console.log('Procesando, espere por favor...');
+                            console.log('Procesando, espere por favor...');
                         },
                         success: function(response) {
                             if (response) {
-                                //   $.notify("La solicitud de canje ha sido almacenada.", "success");
                                 swal("Solicitud de canje", "Tu orden ha sido realizada correctamente", "success");
                                 setTimeout(function() { location.href = "https://www.reconocelo.com.mx"; }, 3000);
-                                //delArray();//Elimina el contenido del array
-                                // setTimeout(function(){location.href="http://www.lealtadsandvik.com.mx";}, 3000);
                             } else {
                                 swal("Error de comunicación", "Ha ocurrido un error de comunicación. Intente nuevamente", "warning");
-                                //   $.notify("Ha ocurrido un error de comunicación. Intente nuevamente.", "error");
                                 $("#btnGenCanje").show();
                                 $("#lblProc").hide();
                             }
                         },
                         error: function(x, e) {
                             swal("Error al realizar el canje", "Ocurrio un error al realizar el canje:" + e.messager, "warning");
-                            // alert("Ocurrio un error al realizar el canje:" + e.messager);
                             $("#btnGenCanje").show();
                             $("#lblProc").hide();
                         }
                     });
 
-
-
-
                 } else {
-                    //  $.notify("No tienes ningun articulo en tu carrito.", "error");  
                     swal("Operacion no permitida", "No tienes ningun articulo en tu carrito", "warning");
-                    // swal("No tienes ningun articulo en tu carrito." + e.messager, "warning");   
                 }
-
-
 
             } else {
                 swal("Operacion no permitida", "Su saldo es insuficiente para realizar este canje.", "warning");
-                //   $.notify("Su saldo es insuficiente para realizar este canje.", "error");
-                //   $("#btnGenCanje").show();
-                //  $("#lblProc").hide();
             }
         } else {
             swal("Operacion no permitida", "No es posible realizar el proceso de canje.", "warning");
-            //$.notify("No es posible realizar el proceso de canje.", "error");
         }
     } else {
         swal("Operacion no permitida", "Todos los campos son obligatorios.", "warning");
-        //$.notify("Todos los campos son obligatorios.", "error");
     }
 }
 
 function exit() {
-
-
-
-
-
 
     swal({
         title: "¿Esta seguro de cerrar sesion?",
@@ -340,21 +253,6 @@ function exit() {
         }
     });
 
-
-
-
-    // bootbox.confirm('message', 'asdas');
-    /* 
-    var r = swal("Are you sure you want to do this?", {
-      buttons: ["Oh noez!", true],
-       });; 
-        if (r == true) {
-           location.href="https://"+location.hostname+"/exit_controller";
-        } else {
-          
-        }
-        */
-
 }
 
 function validaCampos() {
@@ -368,7 +266,6 @@ function validaCampos() {
         } else {
             $("#g" + v.name).removeClass("has-error");
         }
-        //console.log("name:"+v.name+"___VAL:"+v.value);
     });
     if (vc == 1) {
         return false;
@@ -398,28 +295,17 @@ function CambiarCorreo() {
             },
             success: function(response) {
                 if (response) {
-                    //  $.notify("Envio de tu mensaje finalizado correctamente.", "success");
                     swal("Cambio de email", "Cambio de correo exitosamente", "success");
-                    // document.getElementById("mensaje").value = "";
-
-                    //   location.reload();
-                    //delArray();//Elimina el contenido del array
                     //  setTimeout(function(){location.href="https://www.reconocelo.com.mx";}, 3000);
                 } else {
 
                     swal("Cambio de email", "Ha ocurrido un error al cambiar el email.", "warning");
                     // $.notify("Ha ocurrido un error al enviar tu mensaje.", "error");
-                    // swal("Mensaje enviado", "Envio de tu mensaje finalizado correctamente.", "success");  
-                    //  
-                    //  $("#btnGenCanje").show();
-                    // $("#lblProc").hide();
                 }
             },
             error: function(x, e) {
                 swal("Cambio de email", "Ha ocurrido un error al cambiar el email." + e + x, "warning");
                 // alert("Ocurrio un error al enviar su duda" + e.messager);
-                //   $("#btnGenCanje").show();
-                //  $("#lblProc").hide();
             }
         });
 
@@ -431,7 +317,6 @@ function CambiarCorreo() {
 
     return true;
 }
-
 
 function validateEmailLogIn(correo) {
     var pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
