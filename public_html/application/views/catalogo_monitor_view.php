d5ef240ef4a892c62f804cce48616f813bc43689
Armando Ramirez <armando.ramirez@opisa.com>
2018-12-14 12:51:39 -0600

    imagen no encontrada

--- public_html/application/views/catalogo_monitor_view.php
+++ public_html/application/views/catalogo_monitor_view.php
@@ -90,7 +90,7 @@ include 'home_monitor_view_header.php';
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Imagen del premio</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
