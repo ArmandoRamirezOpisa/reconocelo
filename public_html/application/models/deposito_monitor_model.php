3067af9fb090f263206c0a7226eb732e3de9b55b
Armando Ramirez <armando.ramirez@opisa.com>
2018-12-17 15:38:54 -0600

    cambios a monitor-reconocelo

--- public_html/application/models/deposito_monitor_model.php
+++ public_html/application/models/deposito_monitor_model.php
@@ -1,72 +1,72 @@
 <?php
     class Deposito_monitor_model extends CI_Model {
     	
-    	public function __construct()
-    	{
-    		
-    	}
+    	      public function __construct()
+    	      {	
+    	      }
         
-          public function getFechaDeposito()
-          {
+            public function getFechaDeposito()
+            {
                   $query = $this->db->query("
                         SELECT DISTINCT DATE_FORMAT( m.feMov,  '%Y %m' ) AS Fecha
                         FROM PartMovsRealizados m
                         JOIN Participante p ON p.idParticipante = m.idParticipante
                         WHERE p.codEmpresa = ".$this->session->userdata('CodEmpresa')."
                         AND p.CodPrograma =41
-                        AND feMov >=  '20180101'                  
-                                        ");
+                        AND feMov >=  '20180101'
+                        ORDER BY Fecha
+                  ");
                   if ($query->num_rows() > 0)
                   {
-                  return $query->result_array(); 
+                        return $query->result_array(); 
                   }else{
-                  return false;
+                        return false;
                   }
-          }
+            }
 
-        public function getDeposito()
-        {
-    		$query = $this->db->query("
-                                          SELECT m.noFolio, 
-                                          CONCAT( p.PrimerNombre,  ' ', p.SegundoNombre,  ' ', p.ApellidoPaterno,  ' ', 
-                                          p.ApellidoMaterno ) AS Nombre, DATE_FORMAT( m.feMov,  '%Y %m %d' ) AS Fecha, m.dsMov AS Descripcion, 
-                                          m.noPuntos AS Puntos
-                                          FROM PartMovsRealizados m
-                                          JOIN Participante p ON p.idParticipante = m.idParticipante
-                                          WHERE p.codEmpresa = ".$this->session->userdata('CodEmpresa')."
-                                          AND p.CodPrograma =41
-                                          AND feMov >=  '20180101'
-                                        
-                                      ");
-    		if ($query->num_rows() > 0)
-    		{
-                return $query->result_array(); 
-    		}else{
-                return false;
-    		}
-        }
+            public function getDeposito()
+            {
+    		      $query = $this->db->query("
+                        SELECT m.noFolio, 
+                        CONCAT( p.PrimerNombre,  ' ', p.SegundoNombre,  ' ', p.ApellidoPaterno,  ' ', 
+                        p.ApellidoMaterno ) AS Nombre, DATE_FORMAT( m.feMov,  '%Y %m %d' ) AS Fecha, m.dsMov AS Descripcion, 
+                        m.noPuntos AS Puntos
+                        FROM PartMovsRealizados m
+                        JOIN Participante p ON p.idParticipante = m.idParticipante
+                        WHERE p.codEmpresa = ".$this->session->userdata('CodEmpresa')."
+                        AND p.CodPrograma =41
+                        AND feMov >=  '20180101'
+                        ORDER BY Fecha
+                  ");
+    		      if ($query->num_rows() > 0)
+    		      {
+                        return $query->result_array(); 
+    		      }else{
+                        return false;
+    		      }
+            }
 
-        public function getDepositoFechas($infoFechas)
-        {
-    		$query = $this->db->query("
-                                          SELECT m.noFolio, CONCAT( p.PrimerNombre,  ' ', p.SegundoNombre,  ' ', 
-                                          p.ApellidoPaterno,  ' ', p.ApellidoMaterno ) AS Nombre, 
-                                          DATE_FORMAT( m.feMov, '%Y %m %d' ) AS Fecha, m.dsMov AS Descripcion, 
-                                          m.noPuntos AS Puntos
-                                          FROM PartMovsRealizados m
-                                          JOIN Participante p ON p.idParticipante = m.idParticipante
-                                          WHERE p.codEmpresa = ".$this->session->userdata('CodEmpresa')."
-                                          AND p.CodPrograma = 41
-                                          AND DATE_FORMAT( m.feMov,  '%Y %m' ) >= '".$infoFechas['fechaInicio']."'
-                                          AND DATE_FORMAT( m.feMov,  '%Y %m' ) <= '".$infoFechas['fechaFin']."'
-                                        
-                                      ");
-    		if ($query->num_rows() > 0)
-    		{
-                return $query->result_array(); 
-    		}else{
-                return false;
-    		}
-        }
-     }
+            public function getDepositoFechas($infoFechas)
+            {
+    		      $query = $this->db->query("
+                        SELECT m.noFolio, CONCAT( p.PrimerNombre,  ' ', p.SegundoNombre,  ' ', 
+                        p.ApellidoPaterno,  ' ', p.ApellidoMaterno ) AS Nombre, 
+                        DATE_FORMAT( m.feMov, '%Y %m %d' ) AS Fecha, m.dsMov AS Descripcion, 
+                        m.noPuntos AS Puntos
+                        FROM PartMovsRealizados m
+                        JOIN Participante p ON p.idParticipante = m.idParticipante
+                        WHERE p.codEmpresa = ".$this->session->userdata('CodEmpresa')."
+                        AND p.CodPrograma = 41
+                        AND DATE_FORMAT( m.feMov,  '%Y %m' ) >= '".$infoFechas['fechaInicio']."'
+                        AND DATE_FORMAT( m.feMov,  '%Y %m' ) <= '".$infoFechas['fechaFin']."'
+                        ORDER BY Fecha
+                  ");
+    		      if ($query->num_rows() > 0)
+    		      {
+                        return $query->result_array(); 
+    		      }else{
+                        return false;
+    		      }
+            }
+      }
 ?>
