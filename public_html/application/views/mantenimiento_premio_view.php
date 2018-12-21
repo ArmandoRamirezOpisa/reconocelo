00d8506455f18e6d71087b31517163be6583170b
Armando Ramirez <armando.ramirez@opisa.com>
2018-12-21 16:15:38 -0600

    pantallas lista

--- public_html/application/views/mantenimiento_premio_view.php
+++ public_html/application/views/mantenimiento_premio_view.php
@@ -28,12 +28,12 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="/mantenimiento/participantes">Participantes</a>
+                        <a class="nav-link" href="/mantenimiento/home">Inicio <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
+                        <a class="nav-link" href="/mantenimiento/participantes">Participantes</a>
+                    </li>
+                    <li class="nav-item active">
                         <a class="nav-link" href="/mantenimiento/premios">Premios</a>
                     </li>
                 </ul>
