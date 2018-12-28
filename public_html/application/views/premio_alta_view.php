df9a9020ef6404951ada8b883747132151c54bd6
Armando Ramirez <armando.ramirez@opisa.com>
2018-12-28 09:56:00 -0600

    alta premio

--- public_html/application/views/premio_alta_view.php
+++ public_html/application/views/premio_alta_view.php
@@ -1,46 +1,58 @@
             <div class="row">
-                    <div class="col-sm"></div>
-                    <div class="col-sm">
-                        <h3>Dar de alta un premio</h3>
-                    </div>
-                    <div class="col-sm"></div>
+                <div class="col-sm"></div>
+                <div class="col-sm">
+                    <h3>Dar de alta un premio</h3>
                 </div>
-                <form>
-                    <div class="form-group">
-                        <label for="codPremio">Codigo de premio</label>
-                        <input type="text" class="form-control" id="codPremio">
-                    </div>
-                    <div class="form-group">
-                        <label for="codCategoria">Codigo categoria</label>
-                        <input type="text" class="form-control" id="codCategoria">
-                    </div>
-                    <div class="form-group">
-                        <label for="codProveedor">Codigo proveedor</label>
-                        <input type="text" class="form-control" id="codProveedor">
-                    </div>
-                    <div class="form-group">
-                        <label for="marca">Marca</label>
-                        <input type="text" class="form-control" id="marca">
-                    </div>
-                    <div class="form-group">
-                        <label for="modelo">Modelo</label>
-                        <input type="text" class="form-control" id="modelo">
-                    </div>
-                    <div class="form-group">
-                        <label for="nomESP">Nombre Espanol</label>
-                        <input type="text" class="form-control" id="nomESP">
-                    </div>
-                    <div class="form-group">
-                        <label for="nomING">Nombre Ingles</label>
-                        <input type="text" class="form-control" id="nomING">
-                    </div>
-                    <div class="form-group">
-                        <label for="caracESP">Caracteristicas espanol</label>
-                        <input type="text" class="form-control" id="caracESP">
-                    </div>
-                    <div class="form-group">
-                        <label for="caracING">Caracteristicas ingles</label>
-                        <input type="text" class="form-control" id="caracING">
-                    </div>
-                    <button type="button" class="btn btn-primary"><i class="fas fa-save"></i>  Guardar</button>
-                </form>
+                <div class="col-sm"></div>
+            </div>
+            <form>
+                <div class="form-group">
+                    <label for="codPremio">Codigo de premio</label>
+                    <input type="text" class="form-control" id="codPremio">
+                </div>
+                <div class="form-group">
+                    <label for="codCategoria">Codigo categoria</label>
+                    <input type="text" class="form-control" id="codCategoria">
+                </div>
+                <div class="form-group">
+                    <label for="codProveedor">Codigo proveedor</label>
+                    <input type="text" class="form-control" id="codProveedor">
+                </div>
+                <div class="form-group">
+                    <label for="marca">Marca</label>
+                    <input type="text" class="form-control" id="marca">
+                </div>
+                <div class="form-group">
+                    <label for="modelo">Modelo</label>
+                    <input type="text" class="form-control" id="modelo">
+                </div>
+                <div class="form-group">
+                    <label for="nomESP">Nombre Espanol</label>
+                    <input type="text" class="form-control" id="nomESP">
+                </div>
+                <div class="form-group">
+                    <label for="nomING">Nombre Ingles</label>
+                    <input type="text" class="form-control" id="nomING">
+                </div>
+                <div class="form-group">
+                    <label for="caracESP">Caracteristicas espanol</label>
+                    <input type="text" class="form-control" id="caracESP">
+                </div>
+                <div class="form-group">
+                    <label for="caracING">Caracteristicas ingles</label>
+                    <input type="text" class="form-control" id="caracING">
+                </div>
+                <div class="form-group">
+                    <label for="codPrograma">Codigo de programa</label>
+                    <input type="text" class="form-control" id="codPrograma">
+                </div>
+                <div class="form-group">
+                    <label for="codEmpresa">Codigo de empresa</label>
+                    <input type="text" class="form-control" id="codEmpresa">
+                </div>
+                <div class="form-group">
+                    <label for="valorPuntos">Valor en puntos</label>
+                    <input type="text" class="form-control" id="valorPuntos">
+                </div>
+                <button type="button" class="btn btn-primary"><i class="fas fa-save"></i>  Guardar</button>
+            </form>
