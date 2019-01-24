c43048a3aa314041abecc531dd47296777a82fef
Armando Ramirez <armando.ramirez@opisa.com>
2019-01-24 15:33:32 -0600

    mejorando monitor reconocelo

--- public_html/application/views/home_monitor_view_header.php
+++ public_html/application/views/home_monitor_view_header.php
@@ -12,7 +12,60 @@
     </head>
     <body ng-controller="monitor_controller">
         <!-- Navigation -->
-        <nav class="navbar navbar-expand-lg fixed-top " >
+        <nav class="navbar navbar-expand-lg">
+            <a id="inicioMonitor" class="navbar-brand" href="javascript:void(0)" onClick = "MonitorNav(this)">
+                <img src="../assets/images/monitorLog.png" width="150" height="30" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars fa-lg"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a id="participantes" class="nav-link" href="javascript:void(0)" onClick = "MonitorNav(this)"><i class="fas fa-users mr-2"></i>Participantes</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-piggy-bank mr-2"></i>Depositos
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a id="depositosPuntos" class="dropdown-item" href="javascript:void(0)" onClick = "MonitorNav(this)"><i class="fas fa-piggy-bank mr-2"></i>Ver Depositos</a>
+                            <div class="dropdown-divider"></div>
+                            <a id="depositosPuntos" class="dropdown-item" href="javascript:void(0)" onClick = "MonitorNav(this)" style="display:none;"><i class="fas fa-piggy-bank mr-2"></i>Insertar Depositos</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a id="catologoActual" class="nav-link" href="javascript:void(0)" onClick = "MonitorNav(this)"><i class="fas fa-book mr-2"></i>Catalogo Actual</a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="monitorPrograma" class="nav-link" href="javascript:void(0)" onClick = "MonitorNav(this)"><i class="fas fa-chart-pie mr-2"></i>Monitor de programa</a>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <div class="btn-group">
+                        <button class="btn btn-secondary btn-lg" type="button">
+                            <i class="fas fa-user-alt mr-2"></i><?php echo $this->session->userdata('empresa'); ?>
+                        </button>
+                        <button type="button" id="dropdownMenuButton" class="btn btn-lg btn-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span class="sr-only">Toggle Dropdown</span>
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="javascript:void(0)" onClick = "exit()"><i class="fas fa-sign-out-alt mr-2"></i>Cerrar session</a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </nav>
+
+
+
+
+
+
+
+
+
+        <!--<nav class="navbar navbar-expand-lg fixed-top " >
             <div class="container" >
                 <a id="inicioMonitor" class="navbar-brand" href="javascript:void(0)" onClick = "MonitorNav(this)">
 
@@ -20,7 +73,7 @@
                 </a>
 
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fas fa-bars fa-lg"></i>  
+                    <i class="fas fa-bars fa-lg"></i>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ml-auto">   
@@ -47,7 +100,7 @@
                     </ul>
                 </div>
             </div>
-        </nav>          
+        </nav>-->      
         <!-- Navigation --> 
         <div class="container">
             <div class="row justify-content-center mb-4 mt-3">
