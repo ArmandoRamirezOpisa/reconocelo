7d947eb5944489ee35bb8e8f83e774076e225ae7
Armando Ramirez <armando.ramirez@opisa.com>
2019-02-12 12:14:27 -0600

    Realizando cambios reconocelo

--- public_html/application/views/prev_cart_view.php
+++ public_html/application/views/prev_cart_view.php
@@ -1,115 +1,74 @@
 <div class="col-md-12">
 	<h2>Contenido de la orden</h2>
 	<div class="table-responsive">
-	  <table class="table ">
-	    
-  <thead class="thead navcolor text-white">
-    <tr>
-      <th scope="col">C&oacute;digo</th>
-      <th scope="col">Nombre</th>
-      <th scope="col">Cantidad</th>
-      <th scope="col">Valor Puntos</th>
-    </tr>
-
-
-	    	
-	    </thead>
-	    <tbody id = "bodyContentCart">
-	    	
+		<table class="table ">  
+  		<thead class="thead navcolor text-white">
+    		<tr>
+      		<th scope="col">C&oacute;digo</th>
+      		<th scope="col">Nombre</th>
+      		<th scope="col">Cantidad</th>
+      		<th scope="col">Valor Puntos</th>
+					<th scope="col">Eliminar</th>
+    		</tr>
+	  	</thead>
+	    <tbody id="bodyContentCart">	
 	    </tbody>
 	  </table>
 	</div>
-    
-    <br>
-
-    </div><br>
-    
-    <!--<div><h4>Direcci&oacute;n de env&iacute;o</h4>-->
-    <?php
-       /* echo $this->session->userdata('calle')."<br>";
-        echo $this->session->userdata('colonia')."<br>";
-        echo $this->session->userdata('cp')."<br>";
-        echo $this->session->userdata('ciudad')."<br>";
-        echo $this->session->userdata('estado')."<br><br>";*/
-    ?>
-    
- 
-    <!--<a href="javascript:void(0)" onclick="loadSection('cart_controller/getCategory','dvSecc')" class=" btn btn-outline-secondary" id="btnGenCanje">Continuar comprando</a>-->
+  <br>
 </div>
-   
+<br>
+</div>
 <div class="row justify-content-center">
-<div class="col-6">
-
-
-<div class="card card-column bg-light mb-3">
-  <div class="card-header"><h2 class= "text-center"><i class="fas fa-map-marker-alt mr-2"></i>Direcci&oacute;n de entrega</h2></div>
-  <div class="card-body">
-    <h5 class="card-title">Ingrese o actualice la direcci&oacute;n a la que ser&aacute;n enviados los productos.</h5>
-    
-
-   <!--ACCIONES Y FOMRULARIO DE DIRECCIONES-->
-    
-		<form id="frmCanjeDir">
-			  <div class="form-group" id="gedo">
-					<input type="text" class="form-control" id="num" name="num" placeholder="Calle y Número" required>
-			  </div>
-			  <div class="form-group" id="gdelmuni">
-					<input type="text" class="form-control" id="col" name="col" placeholder="Colonia" required>	
-			  </div>
-			  <div class="form-group" id="gcol">
-					<input type="text" class="form-control" id="delmuni" name="delmuni" placeholder="Delegación/Municipio" required>
-			  </div>
-			  <div class="form-group" id="gnum">
-					<input type="text" class="form-control" id="edo" name="edo" placeholder="Estado" required>
-			  </div>
-			  <div class="form-group" id="gcp">
-			    <input type="text" class="form-control" id="cp"  maxlength="5" name="cp" placeholder="C.P." required>
-			  </div>
-
-                            <div class="form-group" id="telefono">
-			    <input type="tel" class="form-control" onkeypress="return isNumberKey(event)" id="tel" name="tel" placeholder="telefono" required>
-			  </div>
-  <div class="form-group" id="ref">
-			    <input type="text" class="form-control" id="refer" name="refer" placeholder="Referencia del domicilio" required>
-			  </div>
-
-
-
-		</form>
-
-<div class="d-flex justify-content-end">
-	    <button id="btnGenCanje" onclick="sendCanje(<?php echo $this->session->userdata('puntos'); ?>,totPuntos)" type="button" class="btn btn-outline-secondary" ><i class="fas fa-flag-checkered mr-2"></i>Finalizar compra</button>
-    <!--TERMINA ACCIONES Y FORMULARIO DE DIRECCIÓN-->
-
+	<div class="col-6">
+		<div class="card card-column bg-light mb-3">
+  		<div class="card-header">
+				<h2 class= "text-center">
+					<i class="fas fa-map-marker-alt mr-2"></i>Direcci&oacute;n de entrega
+				</h2>
+			</div>
+  		<div class="card-body">
+    		<h5 class="card-title">
+					Ingrese o actualice la direcci&oacute;n a la que ser&aacute;n enviados los productos.
+				</h5>
+				<form id="frmCanjeDir">
+			  	<div class="form-group" id="gedo">
+						<input type="text" class="form-control" id="num" name="num" placeholder="Calle y Número" required>
+			  	</div>
+			  	<div class="form-group" id="gdelmuni">
+						<input type="text" class="form-control" id="col" name="col" placeholder="Colonia" required>	
+			  	</div>
+			  	<div class="form-group" id="gcol">
+						<input type="text" class="form-control" id="delmuni" name="delmuni" placeholder="Delegación/Municipio" required>
+			  	</div>
+			  	<div class="form-group" id="gnum">
+						<input type="text" class="form-control" id="edo" name="edo" placeholder="Estado" required>
+			  	</div>
+			  	<div class="form-group" id="gcp">
+			    	<input type="text" class="form-control" id="cp"  maxlength="5" name="cp" placeholder="C.P." required>
+			  	</div>
+          <div class="form-group" id="telefono">
+				    <input type="tel" class="form-control" onkeypress="return isNumberKey(event)" id="tel" name="tel" placeholder="telefono" required>
+			  	</div>
+  				<div class="form-group" id="ref">
+			    	<input type="text" class="form-control" id="refer" name="refer" placeholder="Referencia del domicilio" required>
+			  	</div>
+				</form>
+				<div class="d-flex justify-content-end">
+	    		<button id="btnGenCanje" onclick="sendCanje(<?php echo $this->session->userdata('puntos'); ?>,totPuntos)" type="button" class="btn btn-outline-secondary" ><i class="fas fa-flag-checkered mr-2"></i>Finalizar compra</button>
+				</div>
+  		</div>
+		</div>
+	</div>
 </div>
-
-
-
-
-
-
-
-
-  </div>
-</div>
-
-
-
-
-</div>
-
-</div>
- 
 <script>
 	var str = "";
 	var c = 0;
 	var ctd;
 	var totPuntos = 0;
-	
 	$.each(contOrder, function(k,v){
 		totPuntos = totPuntos + v.puntos;
-		if (c == 0)
-		{
+		if (c == 0){
 			ctd = "class = 'warning'"; 
 			c = 1;
 		}else{
@@ -119,7 +78,6 @@
 		str += "<tr "+ctd+"><td>"+v.id+"</td><td>"+v.nombre+"</td><td>"+v.cantidad+"</td><td>"+formatNumber.new(v.puntos)+"</td><td><a style='cursor:pointer' onClick='deleteItem("+v.id+")'><span class='far fa-trash-alt ml-2' aria-hidden='true'></span></a></td></tr>";
 		$("#bodyContentCart").html(str);
 	});
-
 	str = "<tr><td colspan = 3 style ='text-align:right;'><h2>Total:</h2></td><td><h2>"+formatNumber.new(totPuntos)+"</h2></td></tr>";
 	str += "<tr><td border=0><button id=\"addGenCanje\" onclick=\"loadSection('cart_controller/getCategory','dvSecc')\" type=\"button\" class=\"btn btn-outline-secondary\"><i class=\"fas fa-shopping-basket mr-2\"></i>Continuar comprando</button></td></tr>";
 	$("#bodyContentCart").append(str);
--- public_html/application/views/recuperaPasword_view.php
+++ public_html/application/views/recuperaPasword_view.php
@@ -5,97 +5,58 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Reconocelo</title>
-
     <!-- Bootstrap -->
-
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<!--
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!--<link href="assets/css/bootstrap.min.css" rel="stylesheet">-->
     <link href="assets/css/2018Reconocelo.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> 
-
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-<link rel="shortcut icon" href="https://www.reconocelo.com.mx/assets/images/reconocelo.ico" type="image/x-icon" />
-
-
-
+    <link rel="shortcut icon" href="https://www.reconocelo.com.mx/assets/images/reconocelo.ico" type="image/x-icon" />
   </head>
   <body>
- 	
-      
-    
     <div class="container">
       <!--<center><h2>&#161;Gracias por tu participaci&oacute;n&#33;</h2></center>
-	  <div style="text-align:left;float:left;width:50%;">
-	  	<img style="width:100%;height:auto;" src="assets/images/logo.png">
-	  </div>
+	    <div style="text-align:left;float:left;width:50%;">
+	  	  <img style="width:100%;height:auto;" src="assets/images/logo.png">
+	    </div>
       <div style="float:left;width:50%;height:100%">
-      <label style="margin-top:20%;font-size:20px;"></label><b>Puntos Heinz termin&oacute; el 15 de Enero del 2015, muchas gracias por tu participaci&oacute;n y apoyo a nuestro programa.</b></label>
-
+        <label style="margin-top:20%;font-size:20px;"></label><b>Puntos Heinz termin&oacute; el 15 de Enero del 2015, muchas gracias por tu participaci&oacute;n y apoyo a nuestro programa.</b></label>
       </div>-->
-        <div class="row justify-content-center mt-4 mb-4">
-
-            <div class="col-12 col-md-4 mt-4">
-                <img src="https://www.reconocelo.com.mx/assets/images/reconocelo.png" class="img-fluid mt-4" alt="Responsive image">
-
-
-
-            </div>
-
+      <div class="row justify-content-center mt-4 mb-4">
+        <div class="col-12 col-md-4 mt-4">
+          <img src="https://www.reconocelo.com.mx/assets/images/reconocelo.png" class="img-fluid mt-4" alt="Responsive image">
         </div>
-        
-        
-        <div class="row justify-content-center">
-
-
-
-            <div class="col-12 col-md-4">
-            
-             <form class="form-signin" role="form">
-      
-        <div class="form-group">
-            <label class="lblText mt-4" for="usuario"><b>Ingresa tu correo electr&ograve;nico</b></label>
-            <input class="form-control" placeholder="Ingresa tu correo electr&ograve;nico de registro" required="" autofocus="" type="email" name="usuario" id="usuario">
-        </div>
-       
-             
-
-     
-    </form>
-       
-        <a href="javascript:void(0)" onClick="alert('envio');" class="btn btn-primary text-center btn-block">Enviar contraseña</a>
-             <small class="form-text text-muted text-center">  
-Para una correcta visualizaci&ograve;n del sitio se recomienda el uso de
-Chrome, Firefox, Internet Explorer 11 o superio
-</small>
-    <small class="form-text text-muted text-center">  
-             <a href="https://www.reconocelo.com.mx"><h5>Iniciar sesion</h5></a> 
-</small>  
-        
-        
-        
-        
-      </form>
-            
-            
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-12 col-md-4">    
+          <form class="form-signin" role="form">
+            <div class="form-group">
+              <label class="lblText mt-4" for="usuario"><b>Ingresa tu correo electr&ograve;nico</b></label>
+              <input class="form-control" placeholder="Ingresa tu correo electr&ograve;nico de registro" required="" autofocus="" type="email" name="usuario" id="usuario">
             </div>
+          </form>
+          <a href="javascript:void(0)" onClick="alert('envio');" class="btn btn-primary text-center btn-block">Enviar contraseña</a>
+          <small class="form-text text-muted text-center">  
+            Para una correcta visualizaci&ograve;n del sitio se recomienda el uso de
+            Chrome, Firefox, Internet Explorer 11 o superio
+          </small>
+          <small class="form-text text-muted text-center">  
+            <a href="https://www.reconocelo.com.mx"><h5>Iniciar sesion</h5></a> 
+          </small>
+        </form>
+      </div>
     </div>
-        
-     
-
-
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="assets/js/notify.js"></script>
     <script src="assets/js/functions.js"></script>
     <script src="assets/js/login.js"></script>
-    
   </body>
 </html>
