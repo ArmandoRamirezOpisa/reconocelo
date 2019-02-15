d49549086cccc01c09741926de921e195a9376c7
Armando Ramirez <armando.ramirez@opisa.com>
2019-02-15 15:58:43 -0600

    Reconocelo at&t listo

--- public_html/application/models/cart_model.php
+++ public_html/application/models/cart_model.php
@@ -4,7 +4,7 @@
             public function __construct(){}
             
             public function getAwards($idCat){
-                  if(($this->session->userdata('idPart') == 89526) || ($this->session->userdata('Visibilidad')== 1)) {
+                  /*if(($this->session->userdata('idPart') == 89526) || ($this->session->userdata('Visibilidad')== 1)) {
                         $query = $this->db->query("
                               SELECT DISTINCT(p.codPremio) as codPremio,p.Nombre_Esp,p.Caracts_Esp,pp.ValorPuntos
                               FROM Premio p 
@@ -20,20 +20,26 @@
                               return false;
                         }
                   
-                  }
+                  }*/
                   
                   $query = $this->db->query("
-                        SELECT DISTINCT(p.codPremio) as codPremio,p.Nombre_Esp,p.Caracts_Esp,pp.ValorPuntos
-                        FROM Premio p 
-                        INNER JOIN PremioPrograma pp ON pp.codPremio = p.codPremio 
-                        WHERE pp.codPrograma = ". $this->session->userdata('programa')." 
-                        AND pp.CodCategoria = ".$idCat." 
-                        and pp.CodEmpresa = ".$this->session->userdata('empresa')."
-                        and pp.ValorPuntos <= (select saldoActual from Participante 
-                                                where codPrograma = pp.codPrograma 
-                                                and codEmpresa = ".$this->session->userdata('empresa')." 
-                                                and codParticipante = ".$this->session->userdata('participante')." ) 
-                        ORDER BY pp.ValorPuntos DESC,p.codPremio
+                        SELECT DISTINCT (p.codPremio) AS codPremio, p.Nombre_Esp, p.Caracts_Esp, pp.ValorPuntos
+                        FROM Premio p
+                        INNER JOIN PremioPrograma pp ON pp.codPremio = p.codPremio
+                        JOIN Empresa e ON e.codPrograma = pp.codPrograma
+                        AND e.codEmpresa = pp.codEmpresa
+                        WHERE pp.codPrograma =". $this->session->userdata('programa')."
+                        AND pp.CodCategoria =".$idCat."
+                        AND pp.CodEmpresa = ".$this->session->userdata('empresa')."
+                        AND ( e.catalogoVisible =1
+                        OR pp.ValorPuntos <= ( 
+                        SELECT saldoActual
+                        FROM Participante
+                        WHERE codPrograma = pp.codPrograma
+                        AND codEmpresa =".$this->session->userdata('empresa')."
+                        AND codParticipante =".$this->session->userdata('participante')." )
+                        )
+                        ORDER BY pp.ValorPuntos DESC , p.codPremio
                   ");
                   if ($query->num_rows() > 0)
                   {
@@ -57,7 +63,6 @@
                         WHERE cp.esBaja=0 
                         AND  pp.CodEmpresa = ".$this->session->userdata('empresa')."
                         AND pp.codPrograma = ".$this->session->userdata('programa')." 
-                        ".$w. "
                         ORDER BY cp.nbCategoria                              
                   ");
                   if ($query->num_rows() > 0)
