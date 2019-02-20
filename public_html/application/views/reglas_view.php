a589bd0019f29cd47940b90684f0ba244a80b994
Armando Ramirez <armando.ramirez@opisa.com>
2019-02-20 14:50:23 -0600

    cambios

--- public_html/application/views/reglas_view.php
+++ public_html/application/views/reglas_view.php
@@ -6,30 +6,34 @@
     <div class="panel-group" id="accordion">
       <!--Inicio cajas de las reglas, como acordeon -->
       <?php
-        if ($cat){
-          $contando = 0;
-          $collapses = array("collapseOne", "collapseTwo", "collapseThree","collapseT","collapseFour","collapseFive","collapseSix");
-          $longitud = count($collapses);
-          while($contando <=6){
-            foreach($cat as $row){
-              echo '<div class="panel panel-warning">
-                <div class="panel-heading">
-                  <h4 class="panel-title">              
-                    <a data-toggle="collapse" data-parent="#accordion" href="#'.$collapses[$contando].'">'.
-                      $row["regla"].'
-                    </a>
-                  </h4>
-                </div>
-                <div id="'.$collapses[$contando].'" class="panel-collapse collapse in">
-                  <div class="panel-body">
-                    <p>'.$row["descripcionRegla"].'</p>
+        if($cat){
+          $contador = 0;
+          foreach($cat as $row){
+              echo '<div class="card">
+              <div class="card-header" id="heading'.$row['idReglaNombre'].'">
+                  <h2 class="mb-0">
+                      <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#'.$row['idReglaNombre'].'"';
+                      if($contador == 0){
+                          echo ' aria-expanded="false"';
+                      }else{
+                          echo ' aria-expanded="true"';
+                      }
+                      echo' aria-controls="'.$row['idReglaNombre'].'">
+                          '.$row['regla'].'
+                      </button>
+                  </h2>
+              </div>
+              <div id="'.$row['idReglaNombre'].'" class="collapse" aria-labelledby="heading'.$row['idReglaNombre'].'" data-parent="#accordionExample">
+                  <div class="card-body">
+                    '.$row['descripcionRegla'].'
                   </div>
-                </div>
-              </div>';
-              $contando++;
-            }
+              </div>
+          </div>';
+          $contador = $contador + 1;
           }
-        }
+      }
+
+
       ?>
       <!--Fin cajas de las reglas, como acordeon -->
     </div>
