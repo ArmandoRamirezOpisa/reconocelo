0eb9fd72ca36383394fd46a1d723d4cb3cdbc85a
Armando Ramirez <armando.ramirez@opisa.com>
2019-10-14 11:58:02 -0500

    Se borraron elementos que ya no se necesitan

--- www/assets/js/participantes_controller.js
+++
@@ -1,5 +0,0 @@
-angular.module('participantes_controller',[])
-        .controller('participantes',function ($scope){
-           $scope.mensaje ="asdad"; 
-        });
-
--- www/dev/application/models/noticias_model.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class Noticias_model extends CI_Model {
-    	
-    	public function __construct()//
-    	{
-    		
-    	}
-    }
-?>
