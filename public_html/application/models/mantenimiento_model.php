9748e21d02d7ac5ec524eb4daafe509c500767d2
Armando Ramirez <armando.ramirez@opisa.com>
2019-10-28 14:32:03 -0600

    arreglando el ciclo

--- public_html/application/models/mantenimiento_model.php
+++ public_html/application/models/mantenimiento_model.php
@@ -385,11 +385,14 @@
         }
 
         public function insertPremio($valorColumnaPremio3,$valorColumnaPremio4,$valorColumnaPremio5,$valorColumnaPremio6,$valorColumnaPremio7,$valorColumnaPremio8,$valorColumnaPremio9,$valorColumnaPremio10,$valorColumnaPremio11){
-            sleep(10);
             $query = $this->db->query('
                 CALL spu_InsPremio ('.$valorColumnaPremio3.','.$valorColumnaPremio4.','.$valorColumnaPremio5.',"'.$valorColumnaPremio6.'","'.$valorColumnaPremio7.'","'.$valorColumnaPremio8.'","'.$valorColumnaPremio9.'","'.$valorColumnaPremio10.'","'.$valorColumnaPremio11.'");'
             );
-            if($query){return true;}
+            if($query){
+                return true;
+            }else{
+                return false;
+            }
         }
 	}
 ?>
