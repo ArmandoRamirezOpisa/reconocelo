0e45a8ef762fdb8d48099d2ef124a466d30468e5
Armando Ramirez <armando.ramirez@opisa.com>
2021-01-20 12:06:49 -0600

    cambio mysqli

--- application/config/database.php
+++ application/config/database.php
@@ -82,7 +82,7 @@ $db['default'] = array(
 	'username' => 'root',
 	'password' => 'opisadb',
 	'database' => 'opisa_opisa',
-	'dbdriver' => 'mysql',
+	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
