da9bb97fa7637d8a0a9606c22720f345268152d7
Armando Ramirez <armando.ramirez@opisa.com>
2021-01-21 13:24:47 -0600

    prueba

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/reconocelo/';
+$config['base_url'] = 'http://35.236.41.75/reconocelo/';
 
 /*
 |--------------------------------------------------------------------------
