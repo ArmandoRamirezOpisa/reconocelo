10f8a71eb5ae5484d1b5f47b71998e61d4dcb674
Armando Ramirez <armando.ramirez@opisa.com>
2021-01-21 13:50:42 -0600

    prueba

--- application/controllers/Ini.php
+++ application/controllers/Ini.php
@@ -2,10 +2,8 @@
 	class Ini extends CI_Controller {
 		public function Reconocelo(){
             $loginReconoceloData = array(
-				"usuarioReconocelo"=>$_GET["usuarioReconocelo"],
-                "passwordReconocelo"=>$_GET["passwordReconocelo"]
-                /*"usuarioReconocelo"=>$this->input->post('usuarioReconocelo'),
-                "passwordReconocelo"=>$this->input->post('passwordReconocelo')*/
+                "usuarioReconocelo"=>$this->input->post('usuarioReconocelo'),
+                "passwordReconocelo"=>$this->input->post('passwordReconocelo')
             );
 
             $login['result'] = $this->Reconocelo_model1->loginReconocelo($loginReconoceloData);
