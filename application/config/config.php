4e1f82a64aaba723879606fc98f4386e8c90e684
ArmandoRamirezOpisa <43962699+ArmandoRamirezOpisa@users.noreply.github.com>
2021-02-09 14:33:52 -0600

    Problemas

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://35.231.123.181/reconocelo/';
+//$config['base_url'] = 'http://35.231.123.181/reconocelo/';
+$config['base_url'] = 'http://'. $_SERVER['HTTP_HOST'] .'/reconocelo/'; 
 
 /*
 |--------------------------------------------------------------------------
